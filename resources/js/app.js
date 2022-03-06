c0137bd2aad3a1661c72c37ddb23cc1c796b7184
PovilasKorop <povilask007@gmail.com>
2022-03-06 09:46:54 +0200

    Lesson 21 - Sweet Alert

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,10 @@ import { createApp } from 'vue'
 import App from './layouts/App'
 import LaravelVuePagination from 'laravel-vue-pagination';
 import router from './routes/index'
+import VueSweetalert2 from "vue-sweetalert2";
 
 const app = createApp(App)
 app.use(router)
+app.use(VueSweetalert2)
 app.component('Pagination', LaravelVuePagination)
 app.mount('#app')
--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { ref, inject } from 'vue'
 import { useRouter } from 'vue-router'
 
 export default function usePosts() {
@@ -7,6 +7,7 @@ export default function usePosts() {
     const router = useRouter()
     const validationErrors = ref({})
     const isLoading = ref(false)
+    const swal = inject('$swal')
 
     const getPosts = async (
         page = 1,
@@ -46,6 +47,10 @@ export default function usePosts() {
         axios.post('/api/posts', serializedPost)
             .then(response => {
                 router.push({ name: 'posts.index' })
+                swal({
+                    icon: 'success',
+                    title: 'Post saved successfully'
+                })
             })
             .catch(error => {
                 if (error.response?.data) {
@@ -64,6 +69,10 @@ export default function usePosts() {
         axios.put('/api/posts/' + post.id, post)
             .then(response => {
                 router.push({ name: 'posts.index' })
+                swal({
+                    icon: 'success',
+                    title: 'Post saved successfully'
+                })
             })
             .catch(error => {
                 if (error.response?.data) {
