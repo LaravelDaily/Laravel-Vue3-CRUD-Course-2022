7137d34dc5bd25ae8f1931fd3dc1af5a1effe20c
David Lun <mysticcode@gmail.com>
2022-04-08 18:02:52 +0300

    Implements front-end validation

--- resources/js/components/Posts/Edit.vue
+++ resources/js/components/Posts/Edit.vue
@@ -1,11 +1,14 @@
 <template>
-    <form @submit.prevent="updatePost(post)">
+    <form @submit.prevent="submitForm">
         <!-- Title -->
         <div>
             <label for="post-title" class="block font-medium text-sm text-gray-700">
                 Title
             </label>
             <input v-model="post.title" id="post-title" type="text" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+            <div class="text-red-600 mt-1">
+                {{ errors.title }}
+            </div>
             <div class="text-red-600 mt-1">
                 <div v-for="message in validationErrors?.title">
                     {{ message }}
@@ -20,6 +23,9 @@
             </label>
             <textarea v-model="post.content" id="post-content" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></textarea>
             <div class="text-red-600 mt-1">
+                {{ errors.content }}
+            </div>
+            <div class="text-red-600 mt-1">
                 <div v-for="message in validationErrors?.content">
                     {{ message }}
                 </div>
@@ -32,6 +38,9 @@
                 Category
             </label>
             <select-2 v-model="post.category_id" :options="categories" :settings="{ width: '100%' }"></select-2>
+            <div class="text-red-600 mt-1">
+                {{ errors.category_id }}
+            </div>
             <div class="text-red-600 mt-1">
                 <div v-for="message in validationErrors?.category_id">
                     {{ message }}
@@ -63,21 +72,70 @@
 </template>
 
 <script>
-import {onMounted, reactive } from "vue";
+import { onMounted, reactive, watchEffect } from "vue";
 import { useRoute } from "vue-router";
 import useCategories from "../../composables/categories";
 import usePosts from "../../composables/posts";
+import { useForm, useField, defineRule } from "vee-validate";
+import { required, min } from "../../validation/rules"
+
+defineRule('required', required)
+defineRule('min', min);
 
 export default {
     setup() {
+        // Define a validation schema
+        const schema = {
+            title: 'required|min:5',
+            content: 'required|min:50',
+            category_id: 'required'
+        }
+
+        // Create a form context with the validation schema
+        const { validate, errors, resetForm } = useForm({ validationSchema: schema })
+
+        // Define actual fields for validation
+        const { value: title } = useField('title', null, { initialValue: '' });
+        const { value: content } = useField('content', null, { initialValue: '' });
+        const { value: category_id } = useField('category_id', null, { initialValue: '', label: 'category' });
+
         const { categories, getCategories } = useCategories()
-        const { post, getPost, updatePost, validationErrors, isLoading } = usePosts()
+        const { post: postData, getPost, updatePost, validationErrors, isLoading } = usePosts()
+
+        const post = reactive({
+            title,
+            content,
+            category_id,
+            thumbnail: ''
+        })
+
         const route = useRoute()
+
+        function submitForm() {
+            validate().then(form => { if (form.valid) updatePost(post) })
+        }
+
         onMounted(() => {
             getPost(route.params.id)
             getCategories()
         })
-        return { categories, post, validationErrors, isLoading, updatePost }
+
+        // https://vuejs.org/api/reactivity-core.html#watcheffect
+        watchEffect(() => {
+            post.title = postData.value.title
+            post.content = postData.value.content
+            post.category_id = postData.value.category_id
+        })
+
+        return {
+            categories,
+            post,
+            validationErrors,
+            isLoading,
+            updatePost,
+            errors,
+            submitForm,
+        }
     }
 }
 </script>
