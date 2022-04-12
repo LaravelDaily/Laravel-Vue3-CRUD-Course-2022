61181a52677f1a60388ccf65dd4feaf5ebb6badc
David Lun <mysticcode@gmail.com>
2022-04-12 16:14:23 +0300

    Update CategoryResource for your select2

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -9,14 +9,15 @@ class CategoryResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
+            'id'   => $this->id,
+            'text' => $this->name,
         ];
     }
 }
